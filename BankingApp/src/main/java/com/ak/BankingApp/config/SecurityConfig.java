b7bcdbf34f8f3d2e82a3a2df47d3b5227807c94b
Anupam Singh <anucingh9919@gmail.com>
2025-03-03 21:11:38 +0530

    minor fixes

--- BankingApp/src/main/java/com/ak/BankingApp/config/SecurityConfig.java
+++ BankingApp/src/main/java/com/ak/BankingApp/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
