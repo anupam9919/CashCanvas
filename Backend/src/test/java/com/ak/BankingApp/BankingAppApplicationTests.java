0b56c205d0c0fc3611c0f3afd9dc724b229db61e
Anupam Singh <anucingh9919@gmail.com>
2025-03-27 13:47:00 +0530

    Docker Implemented

--- Backend/src/test/java/com/ak/BankingApp/BankingAppApplicationTests.java
+++ Backend/src/test/java/com/ak/BankingApp/BankingAppApplicationTests.java
@@ -1,13 +1,13 @@
-package com.ak.BankingApp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BankingAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.ak.BankingApp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BankingAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
