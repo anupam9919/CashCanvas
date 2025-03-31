aa758171fdfe7ac2ac84bf8e77292b7c2ea1785c
Anupam Singh <anucingh9919@gmail.com>
2025-03-31 18:56:38 +0530

    Frontend origin added

--- Backend/src/main/java/com/ak/BankingApp/config/SecurityConfig.java
+++ Backend/src/main/java/com/ak/BankingApp/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://bankingapp-wine.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
